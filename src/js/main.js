530c6bcd110fd2446cba37b03ce041ae784cf706
Mitriy_Bug <verstkajoom@gmail.com>
2024-05-06 09:59:54 +0300

    correct work

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
 export default function main() {
+  const container = document.querySelector('.container')
+  if (container) {
+    for(let i = 0; i < 16; i++) {
+      const newDiv = document.createElement('div')
+      if (i === 0) {
+        newDiv.classList.add('active', 'item')
+      } else {
+        newDiv.classList.add('item')
+      }
+      container.insertBefore(newDiv, container.firstChild)
+    }
+  }
   function getRandomNumber(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
@@ -6,9 +18,11 @@ export default function main() {
   }
   function activeItem() {
     const items = document.querySelectorAll('.item:not(.active)')
-    const randomNumber = getRandomNumber(0, items.length)
-    const active = document.querySelector('.active').classList.remove('active')
-    items[randomNumber].classList.add('active')
+    if (items.length > 0) {
+      const randomNumber = getRandomNumber(0, items.length)
+      const active = document.querySelector('.active').classList.remove('active')
+      items[randomNumber].classList.add('active')
+    }
   }
-  setInterval(activeItem, 1000)
+  setInterval(activeItem, 2000)
 }
